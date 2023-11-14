0c05d8fb726524437b752e48e16ecd0fc2fdfe6b
kikuchi1983 <kikuchi.n1997@gmail.com>
2023-11-14 13:57:27 +0900

    石川追加分

--- src/main/java/analix/DHIT/service/UserService.java
+++ src/main/java/analix/DHIT/service/UserService.java
@@ -4,12 +4,23 @@ import analix.DHIT.exception.UserNotFoundException;
 import analix.DHIT.model.User;
 import analix.DHIT.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import java.util.Date;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import com.example.demo.dto.UserRequest;
+
+
 
 @Service
+@Transactional(rollbackFor = Exception.class)
 public class UserService {
 
-
+    @Autowired
     private final UserRepository userRepository;
+
+
+
     public UserService(UserRepository userRepository)
     {
         this.userRepository=userRepository;
@@ -21,7 +32,36 @@ public class UserService {
         {
             throw new UserNotFoundException("User Not Found");
         }
-        return user;
+        return user;git checkout ishikawaa
     }
+    /**
+     * ユーザー情報 全検索
+     * @return 検索結果
+     */
+    public List<User> searchAll() {
+        return userRepository.findAll();
+    }
+
+
+
+    /**
+     * ユーザー情報 新規登録
+     * @param user ユーザー情報
+     */
+    public void create(UserRequest userRequest) {
+        Date now = new Date();
+        User user = new User();
+        user.setName(userRequest.getName());
+        user.setEmployeeCode(userRequest.getEmployeeCode());
+        user.setPassword(userRequest.getPassword());
+        user.setRole(userRequest.getRole());
+        user.setIcon(userRequest.getIcon());
+        user.setCreateDate(now);
+        user.setUpdateDate(now);
+        userRepository.save(user);
+
+
+    }
+
 
 }
